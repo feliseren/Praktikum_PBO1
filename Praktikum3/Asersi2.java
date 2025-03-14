1d5615e2ad215b5d3eaf54d254af1175d3757d8a
feliseren <feliseren05@gmail.com>
2025-03-14 22:08:18 +0700

    Asersi2.java

--- Praktikum3/Asersi2.java
+++ Praktikum3/Asersi2.java
@@ -26,3 +26,12 @@ public class Asersi2 {
         System.out.println("keliling lingkaran = " + keliling);
     }
 }
+
+// PERTANYAAN : 
+// secara konsep, ada yang kurang tepat pada program Asersi2 di atas, jelaskan!
+
+// JAWABAN : 
+// ----Asersi Seharusnya Ditempatkan di Konstruktor----
+// Asersi (assert) digunakan untuk memeriksa nilai yang tidak boleh terjadi dalam program.
+// Dalam kode di atas, asersi hanya dilakukan dalam main() sebelum objek dibuat. 
+// Namun, jika ada cara lain untuk membuat objek lingkaran di tempat lain dalam program, asersi tersebut tidak akan berlaku. Perbaikan kode:
